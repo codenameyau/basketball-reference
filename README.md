5064fce3fb391e9e25880314439fea13ccfb8603
Jorge Yau <jyau01@citymail.cuny.edu>
2015-05-11 00:17:37 -0400

    Added scrapper source file

--- README.md
+++ README.md
@@ -4,6 +4,11 @@ Web scraper for basketball-reference.com. This scraper was built to provide a
 free alternative for collecting NBA data. Building a web scraper with node+cheerio
 is painful. But boy is it fast as fuck--not to mention asynchronous.
 
+### Installation
+```
+npm install basketball-reference --save
+```
+
 ### Quickstart
 ```javascript
 var scraper = require('basketball-reference');
---
+++ src/scraper.js
@@ -0,0 +1,132 @@
+/*!
+ * basketball-reference
+ * MIT License (c) 2015
+ * https://github.com/codenameyau/basketball-reference
+ *
+ * Description:
+ * Scraper for basketball-reference.com that outputs JSON.
+ */
+'use strict';
+
+
+/***************************************************************
+* Dependencies
+***************************************************************/
+var request = require('request');
+var cheerio = require('cheerio');
+var util = require('util');
+
+
+/***************************************************************
+* Globals
+***************************************************************/
+var BASE_URL = 'http://www.basketball-reference.com/';
+
+
+/***************************************************************
+* Internal Functions
+***************************************************************/
+var parseConferenceName = function($conference) {
+  return $conference.find('.sort_default_asc').html();
+};
+
+var parseDivisionName = function($row) {
+  return $row.find('.black_text')[0].children[0].data;
+};
+
+var parseSeed = function(text) {
+  return parseInt(
+    text.match(/\(\d+\)/gm)[0]
+        .match(/\d+/gm)[0], 10);
+};
+
+var parseNumber = function(value) {
+  if (isNaN(value)) {
+    return 0;
+  } else {
+    return parseFloat(value);
+  }
+};
+
+var parseStanding = function($row) {
+  // Preview of JSON.
+  var standing = {};
+
+  // Get team id and name from url.
+  var teamURL = $row.find('a');
+  standing.id = teamURL.attr().href.match(/[A-Z]+/g)[0];
+  standing.team = teamURL.text();
+
+  // Get team standings from raw data.
+  var rawData = $row.text().trim()
+                .replace(/\n\s+/gm, ',')
+                .split(',');
+
+  // Keys for JSON standing.
+  var keys = [
+    'seed',
+    'wins',
+    'loses',
+    'win_percentage',
+    'games_behind',
+    'points_per_game',
+    'points_allowed',
+    'simple_rating_system',
+  ];
+
+  // Seed requires its own parser.
+  standing.seed = parseSeed(rawData[0]);
+  for (var i=1, len=rawData.length; i<len; i++) {
+    standing[keys[i]] = parseNumber(rawData[i]);
+  }
+  return standing;
+};
+
+var addConferenceStandings = function(standings, $, conference) {
+  var $conference = $(conference);
+  var conferenceName = parseConferenceName($conference);
+  var divisionName;
+
+  // Parse table information.
+  $conference.find('tbody tr').each(function(i, row) {
+    var $row = $(row);
+
+    // Table header for Division.
+    if ($row.hasClass('partial_table')) {
+      divisionName = parseDivisionName($row);
+    }
+
+    // Table row for team standings.
+    else if ($row.hasClass('full_table')) {
+      var team = parseStanding($row);
+      team.conference = conferenceName;
+      team.division = divisionName;
+      standings.push(team);
+    }
+  });
+};
+
+var parseLeagueStandings = function(body) {
+  var standings = [];
+  var $ = cheerio.load(body);
+  addConferenceStandings(standings, $, '#E_standings');
+  addConferenceStandings(standings, $, '#W_standings');
+  return standings;
+};
+
+
+/***************************************************************
+* Export Modules
+***************************************************************/
+exports.getLeagueStandings = function(year, cb) {
+  var url = util.format('%s/%s/NBA_%d.html', BASE_URL, 'leagues', year);
+  request.get(url, function(error, res, body) {
+    if (!error && res.statusCode === 200) {
+      // Create cheerio here if reusing body.
+      cb(JSON.stringify({
+        year: year,
+        standings: parseLeagueStandings(body),
+      }, null, ' '));
+    }
+  });
+};
